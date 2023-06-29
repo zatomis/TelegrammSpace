ed30e97b1c935d314c351a0b6ae9a46f9d4f3c32
zatomis <zatomis@list.ru>
2023-06-29 16:16:55 +0300

    Коррекция загрузок фото

--- spacex_last_launches.py
+++ spacex_last_launches.py
@@ -9,18 +9,25 @@ def get_extension(url):
     return os.path.splitext(urlparse(url).path)[1]
 
 
+# def download_photos_of_launches():
+#     url = "https://api.spacexdata.com/v3/launches/"
+#     response = requests.get(url)
+#     response.raise_for_status()
+#     for flight_number in range(1,len(response.json())):
+#         response = requests.get(urljoin(url, str(flight_number)))
+#         response.raise_for_status()
+#         links = response.json()["links"]['flickr_images']
+#         for link_number, link in enumerate(links, start=1):
+#             save.save_photo(link, f"image{flight_number}{link_number}.jpg", "LAUNCH")
+
 def download_photos_of_launches():
     url = "https://api.spacexdata.com/v3/launches/"
     response = requests.get(url)
     response.raise_for_status()
-    for flight_number in range(1,len(response.json())):
-        response = requests.get(urljoin(url, str(flight_number)))
-        print(urljoin(url, str(flight_number)))
-        response.raise_for_status()
-        links = response.json()["links"]['flickr_images']
-        print(links)
-        for link_number, link in enumerate(links, start=1):
-            save.save_photo(link, f"image{flight_number}{link_number}.jpg", "LAUNCH")
+    launches = response.json()
+    for launch in launches:
+        for index, space_url in enumerate(launch["links"]['flickr_images']):
+            save.save_photo(space_url, f"image{index}.jpg", "LAUNCH")
 
 
 if __name__ == '__main__':
